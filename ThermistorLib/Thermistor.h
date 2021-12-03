ab5b2e51a1fc1984b0ab54416cb5a9cf26eee756
Lorenzo Canciani <76554746+cancianilorenzo@users.noreply.github.com>
2021-12-03 15:11:38 +0100

    Add Thermistor lib
    Sketch di base con libreria compila, è da testare se effettivamente porta fuori valori coerenti (il sensore andrà sicuramente calibrato). Fa 100 misurazioni prima di smettere di dare corrente al sensore, capiremo effettivamente quante farne fare e che valori prendere (media? ultimo?) ;)

---
+++ ThermistorLib/Thermistor.h
@@ -0,0 +1,23 @@
+/*
+  Morse.h - Library for flashing Morse code.
+  Created by David A. Mellis, November 2, 2007.
+  Released into the public domain.
+*/
+
+#ifndef Thermistor_h
+#define Thermistor_h
+
+#include "Arduino.h"
+
+class Thermistor
+{
+  public:
+    Thermistor(int pin);
+    double conversion(int value);
+    void getTemp();
+  private:
+    int _pin;
+};
+
+#endif
+
