3fe52bf9e858246f5037171827576ab22ea1849d
Lorenzo Canciani <76554746+cancianilorenzo@users.noreply.github.com>
2021-12-03 21:50:03 +0100

    Add libraries (Photoresistor + Thermistor)

---
+++ Libraries/PhotoresistorLib/Photoresistor.cpp
@@ -0,0 +1,28 @@
+#include "Arduino.h"
+#include "Photoresistor.h"
+
+Photoresistor::Photoresistor(int pin)
+{
+  pinMode(pin, OUTPUT); //Set output to get out power
+    _pin = pin;
+}
+
+double Photoresistor::conversion(int value)
+{
+float voltage = float(value) * (3.3 / float(1023)); //3.3V? con 5V va --> TEST
+float resistence = (1000 * (3.3 - voltage))/voltage; //1000 resistenza, se cambiamo res cambiamo par
+int lux=500/(resistence/1000); 
+return lux; 
+}
+
+void Photoresistor::getLux()
+{
+  digitalWrite(_pin, LOW);
+  for (int i = 0; i < 100; i++) {
+    double Lux = conversion(analogRead(A0));
+    Serial.println(Lux);
+  }
+  digitalWrite(_pin, HIGH);
+  delay(10000);
+}
+
