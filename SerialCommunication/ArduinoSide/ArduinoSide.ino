997cf1995800759f1a82a081b180a17280ac9266
Lorenzo Canciani <76554746+cancianilorenzo@users.noreply.github.com>
2021-12-12 12:45:01 +0100

    Fixed SerialCommunication
    Necessita etichette sensori

--- SerialCommunication/ArduinoSide/ArduinoSide.ino
+++ SerialCommunication/ArduinoSide/ArduinoSide.ino
@@ -1,33 +1,97 @@
-#include <SoftwareSerial.h>
+/*#include <SoftwareSerial.h>
 
-SoftwareSerial espSerial(5, 6); //Pin comunicazione seriale
-String str;
-void setup(){
-Serial.begin(115200);
-espSerial.begin(115200);
-delay(2000);
+  void sendFloat(float f){
+  byte * b = (byte *) &f;
+  Serial.print("f:");
+  Serial.write(b[0]);
+  Serial.write(b[1]);
+  Serial.write(b[2]);
+  Serial.write(b[3]);
+  //Serial.print(68); //Send nonsense.. Else serial drops offline??
+  Serial.flush();
+  return;
+  }
+
+  SoftwareSerial espSerial(5, 6); //Pin comunicazione seriale RX,TX
+  String str;
+  void setup(){
+  Serial.begin(115200);
+  //espSerial.begin(115200);
+  delay(2000);
+  }
+  void loop()
+  {
+  float temp = analogRead(A0); //Thermistor
+  float lux = analogRead(A1); //Photoresistor
+  float water = analogRead(A2); //WaterLevel
+  float soilHum = analogRead(A3); // SoilHum
+
+  /*Serial.print("TEMP: ");
+  Serial.print(temp);
+  Serial.print("---"); //terminatore di stringa, non confonde con -
+  Serial.print(" LUX: ");
+  Serial.print(lux);
+  Serial.println("---");
+  Serial.print(" WATER: ");
+  Serial.print(water);
+  Serial.println("---");
+  Serial.print(" SOILHUM: ");
+  Serial.print(soilHum);
+  Serial.println("---");*/
+//TEMP--LUX--WATER--SOILHUM
+//str =String("_")+String(temp)+String("_")+String(lux)+String("_")+String(water)+String("_")+String(soilHum);
+//espSerial.println(str);
+/*
+  Cast string to char*
+
+
+  //char* cString = (char*) malloc(sizeof(char)*(str.length() + 1));
+  //str.toCharArray(cString, str.length() + 1);
+  /*Serial.println(temp);
+  delay(1000);
+  Serial.println(lux);
+  delay(1000);
+  Serial.println(water);
+  delay(1000);
+  Serial.println(soilHum);
+  delay(1000);
+
+  sendFloat(temp);
+  delay(10000);
+  sendFloat(lux);
+  delay(10000);
+  sendFloat(water);
+  delay(10000);
+  sendFloat(soilHum);
+  delay(10000);
+
+
+  delay(15000);
+  }*/
+
+
+//float val = 1.234;
+
+void setup() {
+  Serial.begin(115200);
 }
-void loop()
-{
-float temp = analogRead(A0); //Thermistor
-float lux = analogRead(A1); //Photoresistor
-float water = analogRead(A2); //WaterLevel
-float soilHum = analogRead(A3); // SoilHum
 
-/*Serial.print("TEMP: ");
-Serial.print(temp);
-Serial.print("---"); //terminatore di stringa, non confonde con -
-Serial.print(" LUX: ");
-Serial.print(lux);
-Serial.println("---");
-Serial.print(" WATER: ");
-Serial.print(water);
-Serial.println("---");
-Serial.print(" SOILHUM: ");
-Serial.print(soilHum);
-Serial.println("---");*/
-//TEMP--LUX--WATER--SOILHUM 
-str =String("_")+String(temp)+String("_")+String(lux)+String("_")+String(water)+String("_")+String(soilHum);
-espSerial.println(str);
-delay(1000);
+void loop() {
+
+
+
+
+  float temp = analogRead(A0); //Thermistor
+  Serial.write((byte*)&temp, 4);
+  delay(3000);
+  float lux = analogRead(A1); //Photoresistor
+  Serial.write((byte*)&lux, 4);
+  delay(3000);
+  float water = analogRead(A2); //WaterLevel
+  Serial.write((byte*)&water, 4);
+  delay(3000);
+  float soilHum = analogRead(A3); // SoilHum
+  Serial.write((byte*)&soilHum, 4);
+  delay(3000);
+
 }
--- SerialCommunication/NodeSide/NodeSide.ino
+++ SerialCommunication/NodeSide/NodeSide.ino
@@ -1,6 +1,4 @@
-
-
-const int NUMSENS = 4;
+/*const int NUMSENS = 4;
 String sensValue[NUMSENS];
 int sensPos = 0; //Posizione nell'array
 String data;
@@ -13,21 +11,59 @@ void setup() {
 }
 void loop() { // run over and over
   if (Serial.available()) {
-    valori = String(Serial.read());
-    Serial.println(valori);
+    byte indata[4] = { 0 };
+    indata[0] = Serial.read();
+    indata[1] = Serial.read();
+    indata[2] = Serial.read();
+    indata[3] = Serial.read();
+
+    float g;
+    memcpy(&g, &indata, sizeof(g));
+
+    Serial.println(g);
   }
 
-  char str_array[valori.length()];
-  valori.toCharArray(str_array, valori.length());
 
-  data = strtok(str_array, "_");
-  while (data != NULL)
-  {
+
+  /*while (Serial.available()) {
+    character = Serial.read();
+    valori.concat(character);
+    }*/
+
+
+  /*char str_array[valori.length()];
+    valori.toCharArray(str_array, valori.length());
+
+    data = strtok(str_array, "_");
+    while (data != NULL)
+    {
     //printf("%s\n", data);
+    Serial.println(data);
     sensValue[sensPos] = data;
     sensPos++;
     data = strtok(NULL, "_");
+    }
+}*/
+
+
+
+void setup() 
+{
+  Serial.begin(115200);
+}
+void loop() 
+{
+  if(Serial.available() > 0)
+  {
+     delay(5);  // wait for all 4 bytes
+     byte buf[4];
+     byte* bp = buf;
+     while (Serial.available()) {
+        *bp = Serial.read();
+        if (bp - buf < 3) bp++;
+     }  
+     float received = * (float*)buf;
+     Serial.println(received, 3); // printing the result to the serial monitor
   }
-
-
+  delay(100); // not really required, should be smaller than sender cycle that is set to 3k
 }
