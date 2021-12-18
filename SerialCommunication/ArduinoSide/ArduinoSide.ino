e767c0a8209ba0328be50ac2f6e40fd15477945e
Lorenzo Canciani <76554746+cancianilorenzo@users.noreply.github.com>
2021-12-18 16:06:33 +0100

    Calibrati sensori luce e temp

--- SerialCommunication/ArduinoSide/ArduinoSide.ino
+++ SerialCommunication/ArduinoSide/ArduinoSide.ino
@@ -1,110 +1,122 @@
-/*#include <SoftwareSerial.h>
-
-  void sendFloat(float f){
-  byte * b = (byte *) &f;
-  Serial.print("f:");
-  Serial.write(b[0]);
-  Serial.write(b[1]);
-  Serial.write(b[2]);
-  Serial.write(b[3]);
-  //Serial.print(68); //Send nonsense.. Else serial drops offline??
-  Serial.flush();
-  return;
-  }
-
-  SoftwareSerial espSerial(5, 6); //Pin comunicazione seriale RX,TX
-  String str;
-  void setup(){
-  Serial.begin(115200);
-  //espSerial.begin(115200);
-  delay(2000);
-  }
-  void loop()
-  {
-  float temp = analogRead(A0); //Thermistor
-  float lux = analogRead(A1); //Photoresistor
-  float water = analogRead(A2); //WaterLevel
-  float soilHum = analogRead(A3); // SoilHum
-
-  /*Serial.print("TEMP: ");
-  Serial.print(temp);
-  Serial.print("---"); //terminatore di stringa, non confonde con -
-  Serial.print(" LUX: ");
-  Serial.print(lux);
-  Serial.println("---");
-  Serial.print(" WATER: ");
-  Serial.print(water);
-  Serial.println("---");
-  Serial.print(" SOILHUM: ");
-  Serial.print(soilHum);
-  Serial.println("---");*/
-//TEMP--LUX--WATER--SOILHUM
-//str =String("_")+String(temp)+String("_")+String(lux)+String("_")+String(water)+String("_")+String(soilHum);
-//espSerial.println(str);
-/*
-  Cast string to char*
-
-
-  //char* cString = (char*) malloc(sizeof(char)*(str.length() + 1));
-  //str.toCharArray(cString, str.length() + 1);
-  /*Serial.println(temp);
-  delay(1000);
-  Serial.println(lux);
-  delay(1000);
-  Serial.println(water);
-  delay(1000);
-  Serial.println(soilHum);
-  delay(1000);
-
-  sendFloat(temp);
-  delay(10000);
-  sendFloat(lux);
-  delay(10000);
-  sendFloat(water);
-  delay(10000);
-  sendFloat(soilHum);
-  delay(10000);
-
-
-  delay(15000);
-  }*/
-
-
-//float val = 1.234;
-
-
- 
- 
 #include "SoftwareSerial.h"
 
-SoftwareSerial Lux(7,8); // rx , tx
-SoftwareSerial Water(2,3) ; // rx , tx 
-SoftwareSerial SoilHum(4,5); // rx , tx
+SoftwareSerial Lux(7, 8); // rx , tx
+SoftwareSerial Water(2, 3) ; // rx , tx
+SoftwareSerial SoilHum(4, 5); // rx , tx
 
+float TempConversion(float value) {
+  float tempK = log(10000.0 * ((1024.0 / value - 1))); //10K is the resistor value!!
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
+  float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
+  return tempC;
+}
+
+float LuxConversion (int value)
+{
+  double Vout=value*0.0048828125;
+  int lux=500/(10*((5-Vout)/Vout));
+  return lux;
+}
+
+
+// variables:
+float temp = 0;                     // the sensor value
+
+float lux = 0;                     // the sensor value
+
+float water = 0;                     // the sensor value
+
+float soilHum = 0;                    // the sensor value
+float sensorMinSoilHum = 1023;        // minimum sensor value
+float sensorMaxSoilHum = 0;           // maximum sensor value
 
 void setup() {
   Serial.begin(115200);
   Lux.begin(115200);
   Water.begin(115200);
   SoilHum.begin(115200);
+
+  // turn on LED to signal the start of the calibration period:
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // calibrate during the first five seconds
+  while (millis() < 5000) {
+    soilHum = analogRead(A3);
+
+    // record the maximum sensor value
+    if (soilHum > sensorMaxSoilHum) {
+      sensorMaxSoilHum = soilHum;
+    }
+
+    // record the minimum sensor value
+    if (soilHum < sensorMinSoilHum) {
+      sensorMinSoilHum = soilHum;
+    }
+  }
+
+  // signal the end of the calibration period
+  digitalWrite(LED_BUILTIN, LOW);
+
 }
+
 
 void loop() {
 
+  float  tempRead = analogRead(A0); //Thermistor
+  Serial.print("TEMP ");
+  /*temp = constrain(temp, sensorMinTemp, sensorMaxTemp);
+    temp = map(temp, sensorMinTemp, sensorMaxTemp, 0, 1023);*/
+  temp = TempConversion(tempRead);
+  Serial.println(temp);
+  //Serial.write((byte*)&temp, 4);
+  delay(3000);
 
 
 
-  float temp = analogRead(A0); //Thermistor
-  Serial.write((byte*)&temp, 4);
+
+  float luxRead = analogRead(A1); //Photoresistor
+  //lux = constrain(lux, sensorMinLux, sensorMaxLux);
+  //lux = map(lux, sensorMinLux, sensorMaxLux, 0, 1023);
+  
+  Serial.print("LUX ");
+  lux = LuxConversion(luxRead);
+  Serial.println(lux);
+  //Lux.write((byte*)&lux, 4);
   delay(3000);
-  float lux = analogRead(A1); //Photoresistor
-  Lux.write((byte*)&lux, 4);
+
+
+
+
+
+
+
+
+
+
+  
+  water = analogRead(A2); //WaterLevel
+  Serial.print("WATER ");
+  Serial.println(water);
+  //Water.write((byte*)&water, 4);
   delay(3000);
-  float water = analogRead(A2); //WaterLevel
-  Water.write((byte*)&water, 4);
-  delay(3000);
-  float soilHum = analogRead(A3); // SoilHum
-  SoilHum.write((byte*)&soilHum, 4);
+
+
+
+
+
+
+
+
+
+
+  
+  soilHum = analogRead(A3); // SoilHum
+  //soilHum = constrain(soilHum, sensorMinSoilHum, sensorMaxSoilHum);
+  soilHum = map(soilHum, sensorMinSoilHum, sensorMaxSoilHum, 0, 100);
+  Serial.print("SOILHUM ");
+  Serial.println(soilHum);
+  //SoilHum.write((byte*)&soilHum, 4);
   delay(3000);
 
 }
