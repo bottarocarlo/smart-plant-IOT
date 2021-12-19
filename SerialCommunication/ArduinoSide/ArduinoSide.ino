72ea422f85b7ec540539b0a3608670f901bc29dc
Lorenzo Canciani <76554746+cancianilorenzo@users.noreply.github.com>
2021-12-19 15:47:18 +0100

    Mappata umidità ambiente
    Mapping SoilHum from 0 to 100
    Percentual value

--- SerialCommunication/ArduinoSide/ArduinoSide.ino
+++ SerialCommunication/ArduinoSide/ArduinoSide.ino
@@ -30,37 +30,19 @@ float lux = 0;                     // the sensor value
 float water = 0;                     // the sensor value
 
 float soilHum = 0;                    // the sensor value
-float sensorMinSoilHum = 1023;        // minimum sensor value
-float sensorMaxSoilHum = 0;           // maximum sensor value
 
 void setup() {
   Serial.begin(115200);
   Lux.begin(115200);
   Water.begin(115200);
   SoilHum.begin(115200);
-
-  // turn on LED to signal the start of the calibration period:
+/*
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
 
-  // calibrate during the first five seconds
-  while (millis() < 5000) {
-    soilHum = analogRead(A3);
-
-    // record the maximum sensor value
-    if (soilHum > sensorMaxSoilHum) {
-      sensorMaxSoilHum = soilHum;
-    }
-
-    // record the minimum sensor value
-    if (soilHum < sensorMinSoilHum) {
-      sensorMinSoilHum = soilHum;
-    }
-  }
-
-  // signal the end of the calibration period
+ 
   digitalWrite(LED_BUILTIN, LOW);
-
+*/
 }
 
 
@@ -116,7 +98,8 @@ void loop() {
   
   soilHum = analogRead(A3); // SoilHum
   //soilHum = constrain(soilHum, sensorMinSoilHum, sensorMaxSoilHum);
-  soilHum = map(soilHum, sensorMinSoilHum, sensorMaxSoilHum, 0, 100);
+  //soilHum = map(soilHum, sensorMinSoilHum, sensorMaxSoilHum, 0, 100);
+  soilHum = map(soilHum, 563, 0, 0, 100);
   Serial.print("SOILHUM ");
   Serial.println(soilHum);
   //SoilHum.write((byte*)&soilHum, 4);
